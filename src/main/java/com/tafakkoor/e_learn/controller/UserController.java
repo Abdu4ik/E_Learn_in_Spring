e5d65834b9a4b1069e92c8d9e01b838079e11f17
Fayzullo <qahramonovf26@gmail.com>
2023-03-23 10:20:46 +0500

    merged

--- src/main/java/com/tafakkoor/e_learn/controller/UserController.java
+++ src/main/java/com/tafakkoor/e_learn/controller/UserController.java
@@ -16,10 +16,7 @@ import java.util.concurrent.CompletableFuture;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
